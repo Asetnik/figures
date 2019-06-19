4bffcda79779863f145c1a3f580694b55919a115
Asetnik <asetnik98@mail.ru>
2019-06-19 09:44:26 +0300

    was created LaravelReact application

---
+++ resources/js/components/App/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ReactDOM, { render } from 'react-dom';
+
+import Test from './../Test/Test';
+
+class App extends Component {
+    render() {
+        return (
+            <Test />
+        );
+    }
+}
+
+export default App;
---
+++ resources/js/components/Test/Test.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class Test extends Component {
+    render() {
+        return (
+            <h3>Test component</h3>
+    );
+    }
+}
+
+export default Test;
